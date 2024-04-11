17df5ca73c0bad9e5272b527de060daaa51e419c
preetidas60 <dasp69833@gmail.com>
2024-04-11 17:27:31 +0530

    Added the problem of Palindrome number

---
+++ src/Problem3.java
@@ -0,0 +1,17 @@
+public class Problem3 {
+    public static void main(String[] args) {
+        int value = 13231;
+        System.out.println(isPalindrome(121));
+    }
+    static boolean isPalindrome(int x) {
+        int rem=0;
+        int num =0;
+        int original = x;
+        while(x>0){
+            rem = x % 10;
+            num = num*10 +rem;
+            x= x/10;
+        }
+        return original == num;
+    }
+}
