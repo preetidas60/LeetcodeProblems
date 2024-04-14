790090e8bb30c71418d7b11694568b9985c1970a
preetidas60 <dasp69833@gmail.com>
2024-04-14 12:58:16 +0530

    added problem no. 12

---
+++ src/lengthlastword.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class lengthlastword {
+    public static void main(String[] args) {
+        String str = " Hello my name is Preeti ";
+        System.out.println(lengthOfLastWord(str));
+    }
+    static int lengthOfLastWord(String s) {
+        int count = 0;
+        String str = s.trim();
+        for(int i=str.length()-1; i>=0; i--){
+            if(str.charAt(i)!= ' '){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
