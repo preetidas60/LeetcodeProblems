c1431f3821a61cf3c000b5a476b8e171d8b8ebc6
preetidas60 <dasp69833@gmail.com>
2024-04-19 21:11:42 +0530

    added problem no. 25

---
+++ src/SplitArrLargeSum.java
@@ -0,0 +1,35 @@
+public class SplitArrLargeSum {
+    public static void main(String[] args) {
+        int[] arr = {7,2,5,10,8};
+        System.out.println(splitArray(arr,2));
+    }
+    static int splitArray(int[] nums, int k) {
+        int start =0;
+        int end =0;
+        for(int i=0; i<nums.length; i++){
+            start = Math.max(start, nums[i]);
+            end += nums[i];
+        }
+        while(start< end){
+            int mid = start + (end -start)/2;
+            int sum=0;
+            int pieces= 1;
+            for(int num : nums){
+                if(sum + num > mid){
+                    sum = num;
+                    pieces++;
+                }
+                else{
+                    sum +=num;
+                }
+            }
+            if(pieces > k){
+                start = mid+1;
+            }
+            else{
+                end = mid;
+            }
+        }
+        return end;
+    }
+}
