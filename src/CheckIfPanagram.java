d40c132551cf67d846a9bc00ae56faecacd6ff63
preetidas60 <dasp69833@gmail.com>
2024-04-22 13:27:13 +0530

    added problem no. 32

---
+++ src/CheckIfPanagram.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+public class CheckIfPanagram {
+    public static void main(String[] args) {
+        String name = "acdefghijklmnopqrstuvwxyz";
+        System.out.println(checkIfPangram(name));
+    }
+    static boolean checkIfPangram(String sentence) {
+        HashSet<Character> mySet = new HashSet<>();
+        for(int i='a'; i<='z'; i++){
+            mySet.add((char)i);
+        }
+        for(int i=0; i<sentence.length(); i++){
+            mySet.remove(sentence.charAt(i));
+            if(mySet.isEmpty()) return true;
+        }
+        return false;
+    }
+
+//    Efficient code
+//    static boolean checkIfPangram(String sentence) {
+//            boolean[] letters  = new boolean[26];
+//
+//            for(char c : sentence.toCharArray()) {
+//                letters[c - 'a'] = true;
+//            }
+//
+//            //find any letter that not exist
+//            for(boolean existLetter : letters) {
+//                if(!existLetter) return false;
+//            }
+//
+//            return true;
+//        }
+}
