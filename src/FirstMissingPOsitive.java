e1dc2a4dccfc4e6398bac39ff3d629d318d7e8e9
preetidas60 <dasp69833@gmail.com>
2024-05-01 14:47:48 +0530

    optimized TwoSum code

---
+++ src/FirstMissingPOsitive.java
@@ -0,0 +1,20 @@
+public class FirstMissingPOsitive {
+    public static void main(String[] args) {
+        int[] arr = {0,2,2,4,0,1,0,1,3};
+        System.out.println(firstMissingPositive(arr));
+    }
+    static int firstMissingPositive(int[] nums) {
+        boolean[] arr = new boolean[nums.length+1];
+        for(int i=0; i< nums.length; i++){
+            if(nums[i] > 0 && nums[i]<=nums.length){
+                arr[nums[i]] = true;
+            }
+        }
+        for(int i=1; i< arr.length; i++){
+            if(!arr[i]){
+                return i;
+            }
+        }
+        return nums.length+1;
+    }
+}
--- src/TwoSum.java
+++ src/TwoSum.java
@@ -2,18 +2,18 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {2,7,11,15};
-        int add[] = (twoSum(arr, 9));
+        int[] arr = {3,2,7,11,3};
+        int add[] = (twoSum(arr, 6));
         System.out.println(Arrays.toString(add));
     }
         static int[] twoSum(int[] nums, int target) {
-            for(int i=0;i<(nums.length-1);i++){
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[i]+nums[j]==target){
-                        return new int[]{i,j} ;
+            for(int i=1;i<(nums.length);i++){
+                for(int j=i;j<nums.length;j++){
+                    if(nums[j]+nums[j-i] == target){
+                        return new int[]{j-i, j} ;
                     }
                 }
             }
-            return new int[]{};
+            return null;
         }
 }
